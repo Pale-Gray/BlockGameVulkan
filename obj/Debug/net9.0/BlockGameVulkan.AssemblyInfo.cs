1136b85f2e15d70456459009575c5ef9d0ededd0
pale <73047946+Pale-Gray@users.noreply.github.com>
2025-03-16 23:12:59 -0400

    update

--- obj/Debug/net9.0/BlockGameVulkan.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlockGameVulkan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlockGameVulkan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c636c1232c194e51d5e352468f71b6e8b4511ec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlockGameVulkan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlockGameVulkan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
